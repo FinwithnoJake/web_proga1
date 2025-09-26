db1a3572c3cef43e76efa5758c35ac75c5cb5ddf
Fin <lizakolodei84@gmail.com>
2025-09-26 12:27:19 +0300

    1

---
+++ src/main/java/main/Validator.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Validator {
+    //radio
+    public static boolean validateX(int x) {
+        return x >= -4 && x <= 4;
+    }
+
+    //text
+    public static boolean validateY(float y) {
+        return y >= -3 && y <= 5;
+    }
+
+    //button
+    public static boolean validateR(int r) {
+        return r == 1 || r == 1.5 || r == 2 || r == 2.5 || r == 3;
+    }
+}
---
+++ src/static/js/index.js
@@ -0,0 +1,71 @@
+$(".custom-button").on("click", function() {
+        // Убираем класс "active" у всех кнопок
+        $(".custom-button").removeClass("active-button");
+        // Добавляем класс "active" к нажатой кнопке
+        $(this).addClass("active-button");
+        });
+
+        $("#submit").on("click", function() {
+        let json = {
+        "x": $("input[name='xValue']:checked").val(),
+        "y": $("input[name='yValue']").val(),
+        "r": $(".active-button").val()
+        };
+        console.log(json.x, json.y, json.r)
+        if (isNaN(+json.x) || isNaN(+json.y) || isNaN(json.r) || json.y < -3 || json.y > 5) {
+        alert("Некорректные данные")
+            if (!json.x) {
+                alert("Пожалуйста, выберите значение x");}
+            if (!json.y) {
+                alert("Пожалуйста, выберите значение y");}
+            if (!json.r) {
+                alert("Пожалуйста, выберите значение r");}
+        }
+        else {
+        startTime = new Date().getTime();
+        fetch("/fcgi-bin/app.jar?" + new URLSearchParams(json), {
+        method: "GET",
+        headers: {
+        "Content-Type": "application/json"
+        }
+        })
+        .then(response => {
+        if (!response.ok) {
+        throw new Error("Network response was not ok");
+        }
+        return response.json();
+        })
+        .then(response => {
+        let odz;
+        if (response.result) {
+        odz = "Попадание";
+        } else {
+        odz = "Мимо";
+        }
+
+        // Время выполнения скрипта
+        let endTime = new Date();
+        let executionTime = (endTime - startTime) + " ms";
+
+        // Текущее время
+        let currentTime = new Date().toLocaleString();
+
+        // Добавляем строку в таблицу
+        let newRow = `
+<tr>
+<td>${json.x}</td>
+<td>${json.y}</td>
+<td>${json.r}</td>
+<td>${odz}</td>
+<td>${currentTime}</td>
+<td>${executionTime}</td>
+</tr>
+        `;
+        document.querySelector("#resultTable tbody").insertAdjacentHTML('beforeend', newRow);
+        })
+        .catch(error => {
+        alert("Ошибка при отправке данных: " + error.message);
+        });
+        }
+
+        });
